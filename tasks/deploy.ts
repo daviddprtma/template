36c50a7b1307d46c3c54513c4a6547df0eca1a25
daviddprtma <davidchristianpratama@gmail.com>
2024-01-10 12:58:26 +0700

    onichain part 2 quest 3 done

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,6 +1,7 @@
 import { getAddress } from "@zetachain/protocol-contracts";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import ZRC20 from "@zetachain/protocol-contracts/abi/zevm/ZRC20.sol/ZRC20.json";
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   if (hre.network.name !== "zeta_testnet") {
@@ -18,19 +19,33 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
 
   const systemContract = getAddress("systemContract", "zeta_testnet");
 
-<<<<<<< HEAD
   const factory = await hre.ethers.getContractFactory("Staking");
-=======
-  const factory = await hre.ethers.getContractFactory("Swap");
->>>>>>> 412a84dca856e2135f943daf97cde448045baabb
-  const contract = await factory.deploy(systemContract);
+  let symbol, chainID;
+  if (args.chain === "btc_testnet") {
+    symbol = "BTC";
+    chainID = 18332;
+  } else {
+    const zrc20 = getAddress("zrc20", args.chain);
+    const contract = new hre.ethers.Contract(zrc20, ZRC20.abi, signer);
+    symbol = await contract.symbol();
+    chainID = hre.config.networks[args.chain]?.chainId;
+    if (chainID === undefined) {
+      throw new Error(`🚨 Chain ${args.chain} not found in hardhat config.`);
+    }
+  }
+
+  const contract = await factory.deploy(
+    `Staking rewards for ${symbol}`,
+    `R${symbol.toUpperCase()}`,
+    chainID,
+    systemContract
+  );
   await contract.deployed();
 
   if (args.json) {
     console.log(JSON.stringify(contract));
   } else {
     console.log(`🔑 Using account: ${signer.address}
-
 🚀 Successfully deployed contract on ZetaChain.
 📜 Contract address: ${contract.address}
 🌍 Explorer: https://athens3.explorer.zetachain.com/address/${contract.address}
@@ -38,7 +53,6 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   }
 };
 
-task("deploy", "Deploy the contract", main).addFlag(
-  "json",
-  "Output in JSON"
-);
+task("deploy", "Deploy the contract", main)
+  .addParam("chain", "Chain ID (use btc_testnet for Bitcoin Testnet)")
+  .addFlag("json", "Output in JSON");
